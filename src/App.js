adc5817661862cb0a3ace2b5e94198dfd2808039
Igor <81867285+ITigor1985@users.noreply.github.com>
2021-12-26 12:05:10 +0200

    123

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import Header from './components/Header'
+import user from './components/Profile/user.json';
+import Profile from './components/Profile/Profile'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Header />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">      
+        <Profile user={user} />
     </div>
   );
 }
