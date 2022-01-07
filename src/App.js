53e7aad532c6b1a981620355916f88d768076d98
Igor <81867285+ITigor1985@users.noreply.github.com>
2022-01-07 21:54:42 +0200

    123

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import user from './components/Profile/user.json';
-import Profile from './components/Profile/Profile'
+import data from './components/Statistics/data.json';
+import Profile from './components/Profile/Profile';
+import Statistics from './components/Statistics/Statistics';
 import './App.css';
 
 function App() {
   return (
     <div className="App">      
         <Profile user={user} />
+        <Statistics title="Upload stats" stats={data} />;
+        
     </div>
   );
 }
